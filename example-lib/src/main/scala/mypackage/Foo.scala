8a9b58a48b86c456a0593427fbe217d6f193b404
Gergely Fábián <gergo.fb@gmail.com>
2020-12-05 00:45:00 +0100

    Reproducing coverage issues for Scala 2.12

--- example-lib/src/main/scala/mypackage/Foo.scala
+++ example-lib/src/main/scala/mypackage/Foo.scala
@@ -2,4 +2,13 @@ package mypackage
 
 object Foo {
   val message = "hello world"
+
+  def testLambdas = {
+    for {
+      a <- List(1)
+      b <- List(2)
+      c <- List(3)
+      d <- List(4)
+    } yield a + b + c + d
+  }
 }
--- example-lib/src/test/scala/mypackage/Test.scala
+++ example-lib/src/test/scala/mypackage/Test.scala
@@ -6,4 +6,8 @@ class TestSuite extends FlatSpec {
   "things" should "work" in {
     assert(Foo.message == "hello world")
   }
+
+  "call lambda test" should "work" in {
+    assert(Foo.testLambdas == List(10))
+  }
 }
